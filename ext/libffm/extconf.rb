a1c327611c15baff2b6b2999ce4cd0a866fc0528
Andrew Kane <andrew@ankane.org>
2021-05-23 15:27:37 -0700

    Improved performance

--- ext/libffm/extconf.rb
+++ ext/libffm/extconf.rb
@@ -2,7 +2,7 @@ require "mkmf-rice"
 
 # -DUSESSE requires sse3
 # TODO test for it or update fork to use __SSE3__
-$CXXFLAGS += " -std=c++17 -march=native"
+$CXXFLAGS += " -std=c++17 $(optflags) -march=native"
 
 apple_clang = RbConfig::CONFIG["CC_VERSION_MESSAGE"] =~ /apple clang/i
 

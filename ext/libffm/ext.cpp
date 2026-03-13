a261bbb60cdea1c1051288515f68339eeb4a8759
Andrew Kane <andrew@ankane.org>
2026-03-13 15:16:08 -0700

    Improved predict method

--- ext/libffm/ext.cpp
+++ ext/libffm/ext.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <string_view>
 
 #include <ffm.h>
 #include <rice/rice.hpp>
@@ -51,7 +52,7 @@ void Init_ext() {
       [](ffm::ffm_model& model, const std::string& test_path) {
         std::ifstream f_in(test_path);
         if (!f_in.is_open()) {
-          throw std::runtime_error("Cannot open file");
+          throw std::runtime_error{"Cannot open file"};
         }
         std::string line;
 
@@ -59,32 +60,34 @@ void Init_ext() {
         while (std::getline(f_in, line)) {
           ffm::vector<ffm::ffm_node> x;
 
-          size_t n = line.find_first_of(" \t");
-          if (n == std::string::npos) {
-            throw std::runtime_error("Invalid line");
+          std::string_view line_view{line};
+
+          size_t n = line_view.find_first_of(" \t");
+          if (n == std::string_view::npos) {
+            throw std::runtime_error{"Invalid line"};
           }
 
           size_t start = n + 1;
 
           while (true) {
-            n = line.find_first_of(" \t", start);
+            n = line_view.find_first_of(" \t", start);
 
-            std::string s = n == std::string::npos ? line.substr(start) : line.substr(start, n - start);
+            std::string_view s = n == std::string_view::npos ? line_view.substr(start) : line_view.substr(start, n - start);
 
             size_t n2 = s.find(':');
-            if (n2 == std::string::npos) {
-              throw std::runtime_error("Invalid line");
+            if (n2 == std::string_view::npos) {
+              throw std::runtime_error{"Invalid line"};
             }
 
             size_t n3 = s.find(':', n2 + 1);
-            if (n3 == std::string::npos) {
-              throw std::runtime_error("Invalid line");
+            if (n3 == std::string_view::npos) {
+              throw std::runtime_error{"Invalid line"};
             }
 
-            // TODO use string_view
-            std::string field_char = s.substr(0, n2);
-            std::string idx_char = s.substr(n2 + 1, n3);
-            std::string value_char = s.substr(n3 + 1);
+            // cannot pass string view to stoi/stof
+            std::string field_char{s.substr(0, n2)};
+            std::string idx_char{s.substr(n2 + 1, n3)};
+            std::string value_char{s.substr(n3 + 1)};
 
             ffm::ffm_node N{
               std::stoi(field_char),
